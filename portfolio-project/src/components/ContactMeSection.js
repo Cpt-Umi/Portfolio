95b60219ec69175160345a2b5273211f676d3028
Umair Khan <umairkhan.2102@gmail.com>
2023-09-16 23:33:27 +0500

    Completed with all I had

--- portfolio-project/src/components/ContactMeSection.js
+++ portfolio-project/src/components/ContactMeSection.js
@@ -1,22 +1,161 @@
-import React, {useEffect} from "react";
-import {useFormik} from 'formik'
+import React from "react";
+import { useFormik } from "formik";
 import {
-    Box,
-    Button,
-    FormControl,
-    FormErrorMessage,
-    FormLabel,
-    Heading,
-    Input,
-    Select,
-    Textarea,
-    VStack,
-  } from "@chakra-ui/react";
-  import * as Yup from 'yup';
-  import FullScreenSection from "./FullScreenSection";
-  import useSubmit from "../hooks/useSubmit"
-  import {useAlertContext} from "../context/alertContext";
-  
-  const ContactMeSection = () => {
-    
-  }
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Select,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+import * as Yup from "yup";
+import FullScreenSection from "./FullScreenSection";
+import useSubmit from "../hooks/useSubmit";
+import { useAlertContext } from "../context/alertContext";
+
+const ContactMeSection = () => {
+  const { isLoading, response, submit } = useSubmit();
+  const { onOpen } = useAlertContext();
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      email: "",
+      type: "Freelance project proposal",
+      comment: "",
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .min(3, "Name should be at least 3 characters")
+        .max(25)
+        .required("Name is required"),
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email is required"),
+    }),
+    onSubmit: async (values) => {
+      try {
+        await submit("/api/submit", values);
+        if (response && response.type === "success") {
+          onOpen("success", response.message);
+          formik.resetForm();
+        } else {
+          onOpen("error", response.message);
+        }
+      } catch (error) {
+        onOpen("error", "Something went wrong, please try again later!");
+      }
+    },
+  });
+
+  return (
+    <FullScreenSection
+      isDarkBackground
+      backgroundColor="#90e0ef"
+      py={16}
+      spacing={8}
+    >
+      <VStack w={1024} p={32} alignItems={"flex-start"}>
+        <Heading as="h1" id="contactme-section" ml="25px">
+          Contact Me
+        </Heading>
+        <Box p={6} rounded="md" w="100%">
+          <form onSubmit={formik.handleSubmit}>
+            <VStack spacing={4} color="blackAlpha.700">
+              <FormControl
+                isInvalid={formik.touched.firstName && formik.errors.firstName}
+              >
+                <FormLabel htmlFor="firstName">Name</FormLabel>
+                <Input
+                  id="firstName"
+                  name="firstName"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.firstName}
+                />
+                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+              </FormControl>
+
+              <FormControl
+                isInvalid={formik.touched.email && formik.errors.email}
+              >
+                <FormLabel htmlFor="email">Email Address</FormLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                />
+                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+              </FormControl>
+
+              <FormControl>
+                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
+                <Select
+                  id="type"
+                  name="type"
+                  color="blackAlpha.800"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.type}
+                >
+                  <option value="hireMe">Freelance project proposal</option>
+                  <option value="openSource">
+                    Open source consultancy session
+                  </option>
+                  <option value="other">Other</option>
+                </Select>
+              </FormControl>
+
+              <FormControl
+                isInvalid={formik.touched.comment && formik.errors.comment}
+              >
+                <FormLabel htmlFor="comment">Your message</FormLabel>
+                <Textarea
+                  id="comment"
+                  name="comment"
+                  height={250}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.comment}
+                />
+                <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
+              </FormControl>
+
+              <Button
+                type="submit"
+                colorScheme="purple"
+                width="full"
+                isLoading={isLoading}
+              >
+                Submit
+              </Button>
+            </VStack>
+          </form>
+        </Box>
+      </VStack>
+      {response && (
+        <Box
+          position="fixed"
+          top="1rem"
+          background={
+            response.type === "success" ? "green.300" : "red.300"
+          }
+          color="white"
+          p="1rem"
+          borderRadius="md"
+        >
+          {response.message}
+        </Box>
+      )}
+    </FullScreenSection>
+  );
+};
+
+export default ContactMeSection;
--- portfolio-project/src/components/Header.js
+++ portfolio-project/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {
   FontAwesomeIcon,
 } from "@fortawesome/react-fontawesome";
@@ -28,21 +28,31 @@ const socials = [
 
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
+  const prevScrollY = useRef(0);
 
   const handleScroll = () => {
-    if (window.scrollY > 0) {
+    const currentScrollY = window.scrollY;
+    if (currentScrollY > 0) {
       setScrolled(true);
     } else {
       setScrolled(false);
+    }
+    
+    if (currentScrollY > prevScrollY.current) {
+      prevScrollY.current = currentScrollY;
+      headerRef.current.style.transform = "translateY(-200px)";
+    } else {
+      prevScrollY.current = currentScrollY;
+      headerRef.current.style.transform = "translateY(0)";
     }
   };
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const handleClick = (anchor) => () => {
     const id = `${anchor}-section`;
@@ -55,13 +65,15 @@ const Header = () => {
     }
   };
 
-  const socialLinks = socials.map((social) => {
+  const socialLinks = socials.map((social, index) => {
     return (
-      <Link to={social.url} className="links" target="_blank">
+      <Link to={social.url} className="links" target="_blank" key={index}>
         <FontAwesomeIcon icon={social.icon} size="2x" />
       </Link>
     );
   });
+
+  const headerRef = useRef(null);
 
   return (
     <Box
@@ -69,7 +81,7 @@ const Header = () => {
       top={0}
       left={0}
       right={0}
-      transform={`translateY(${scrolled ? -200 : 0}%)`} 
+      ref={headerRef}
       transition="transform 0.3s ease-in-out"
       background="#18181b"
     >
@@ -83,8 +95,12 @@ const Header = () => {
           <nav>{socialLinks}</nav>
           <nav>
             <HStack spacing={8}>
-              <Link to='/#projects' onClick={handleClick("projects")}>Projects</Link>
-              <Link to='/#contact-me' onClick={handleClick("contactme")}>Contact</Link>
+              <Link to="/#projects" onClick={handleClick("projects")}>
+                Projects
+              </Link>
+              <Link to="/#contact-me" onClick={handleClick("contactme")}>
+                Contact
+              </Link>
             </HStack>
           </nav>
         </HStack>
